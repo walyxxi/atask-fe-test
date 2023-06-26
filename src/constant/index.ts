0ed2b12810489f787f5baf0c94ec509774ab9b23
Waliyul Ardy <waliyulardy@gmail.com>
2023-06-26 20:51:14 +0800

    add .env variables

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,3 +1,2 @@
-export const githubApiDomain = "https://api.github.com";
-export const githubApiKey =
-  "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";
+export const githubApiDomain = import.meta.env.VITE_GITHUB_API_DOMAIN;
+export const githubApiKey = import.meta.env.VITE_GITHUB_API_KEY;
