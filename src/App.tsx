8e1d395d3ad08c853e733c6a8b8a0cccaf295021
Waliyul Ardy <waliyulardy@gmail.com>
2023-06-26 11:39:46 +0800

    update: ui slicing & ux flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import xMarkIcon from "./assets/icons/x-mark.svg";
 import cevronDownIcon from "./assets/icons/cevron-down.svg";
+import cevronUpIcon from "./assets/icons/cevron-up.svg";
 import startIcon from "./assets/icons/star.svg";
 import { DUMMY_USER_DATA } from "./assets/constant";
-import { User, Repo } from "./model";
+import { User, Repo, Users, UserId } from "./model";
 
 function App() {
+  const [users, setUsers] = useState<Users>(DUMMY_USER_DATA);
   const [search, setSearch] = useState("");
 
-  const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) =>
     setSearch(e.target.value);
-  };
 
   const handleClearSearch = () => setSearch("");
+
+  const handleToggleUser = useCallback((id: UserId) => {
+    setUsers((curr) =>
+      curr.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            repos_open: !item.repos_open,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  }, []);
+
+  const handleKeyDownToogleUser = (
+    e: React.KeyboardEvent<HTMLDivElement>,
+    id: UserId
+  ) => {
+    if (e.key === "Enter") {
+      handleToggleUser(id);
+    }
+  };
 
   return (
     <div className="mx-auto my-0 max-w-xl">
@@ -35,15 +60,21 @@ function App() {
           )}
         </div>
         <button
-          className="w-full py-2 rounded-md bg-blue-500 text-white disabled:bg-slate-300 disabled:cursor-not-allowed"
+          className="w-full py-2 rounded-md bg-blue-500 text-white disabled:bg-slate-300 disabled:cursor-not-allowed focus:outline-blue-600"
           disabled={!search}
         >
           Search
         </button>
-        <div className="w-full flex flex-col">
-          {DUMMY_USER_DATA.map((user: User) => (
-            <div key={user.id}>
-              <div className="w-full px-3 py-2 flex justify-between bg-slate-100 rounded-md border-2 border-slate-100 hover:border-blue-200">
+        {/* <UserSkaleton /> */}
+        <div className="w-full flex flex-col select-none">
+          {users.map((user: User) => (
+            <div key={user.id} className="mb-2">
+              <div
+                tabIndex={0}
+                className="px-3 py-2 flex justify-between bg-slate-100 rounded-md border-2 border-slate-100 hover:border-blue-200 focus:outline-blue-400 cursor-pointer"
+                onClick={() => handleToggleUser(user.id)}
+                onKeyDown={(e) => handleKeyDownToogleUser(e, user.id)}
+              >
                 <div className="flex items-center gap-3">
                   <img
                     src={user.avatar_url}
@@ -53,31 +84,34 @@ function App() {
                   <span className="font-bold">{user.login}</span>
                 </div>
                 <img
-                  src={cevronDownIcon}
+                  src={user.repos_open ? cevronUpIcon : cevronDownIcon}
                   alt="_icon"
                   className="w-6 cursor-pointer"
                 />
               </div>
-              <div className="mt-2 flex flex-col">
-                {user?.repos_data?.map((repo: Repo) => (
-                  <div
-                    key={repo.id}
-                    className="ml-6 px-3 py-2 flex flex-col mb-2 rounded-md bg-slate-300 border-2 border-slate-100 hover:border-blue-200"
-                  >
-                    <div className="flex justify-between">
-                      <div className="font-bold">{repo.name}</div>
-                      <div className="flex gap-1">
-                        <span>{repo.stargazers_count}</span>
-                        <img
-                          src={startIcon}
-                          alt="_icon"
-                          className="w-4 cursor-pointer"
-                        />
+              <div className="flex flex-col">
+                {user.repos_open &&
+                  user?.repos_data?.map((repo: Repo) => (
+                    <a
+                      key={repo.id}
+                      className="ml-6 mt-1 px-3 py-2 flex flex-col rounded-md bg-slate-300 focus:outline-slate-400 cursor-pointer"
+                      href={repo.html_url}
+                      target="_blank"
+                    >
+                      <div className="flex justify-between">
+                        <div className="font-bold">{repo.name}</div>
+                        <div className="flex gap-1">
+                          <span>{repo.stargazers_count}</span>
+                          <img
+                            src={startIcon}
+                            alt="_icon"
+                            className="w-4 cursor-pointer"
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div>{repo.description}</div>
-                  </div>
-                ))}
+                      <div>{repo.description}</div>
+                    </a>
+                  ))}
               </div>
             </div>
           ))}
--- src/model.ts
+++ src/model.ts
@@ -6,12 +6,15 @@ export interface Repo {
   html_url: string;
 }
 
+export type UserId = number;
+
 export interface User {
   login: string;
-  id: number;
+  id: UserId;
   avatar_url: string;
   repos_url: string;
   repos_data?: Array<Repo>;
+  repos_open: boolean;
 }
 
 export type Users = Array<User>;
